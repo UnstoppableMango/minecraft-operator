19ed25402433612b3eb1b91bf504901d772d0d5b
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-02-25 21:43:39 -0600

    Some docker stuff

--- hack/custom.mk
+++ hack/custom.mk
@@ -4,6 +4,17 @@ LOCAL_BIN   := ${WORKING_DIR}/bin
 DEVCTL ?= ${LOCALBIN}/devctl
 DPRINT ?= ${LOCALBIN}/dprint
 
+BUN := bun --cwd packages/ui
+
+start-ui:
+	$(BUN) start
+
+dev-ui:
+	$(BUN) dev
+
+docker-build-ui:
+	docker build packages/ui -t minecraft-operator-ui:latest
+
 bin/dprint: .versions/dprint | .make/dprint/install.sh bin
 	DPRINT_INSTALL=${CURDIR} .make/dprint/install.sh $(shell $(DEVCTL) v dprint)
 	@touch $@
