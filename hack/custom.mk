0d6bc6a0b8c074b9d782e82be27877c7049a65b7
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-02-26 23:28:43 -0600

    Buncha boilerplate

--- hack/custom.mk
+++ hack/custom.mk
@@ -1,24 +1,34 @@
+_ != mkdir -p .make
+
 WORKING_DIR != git rev-parse --show-toplevel
 LOCAL_BIN   := ${WORKING_DIR}/bin
 
-DEVCTL ?= ${LOCALBIN}/devctl
-DPRINT ?= ${LOCALBIN}/dprint
+DEVCTL ?= ${LOCAL_BIN}/devctl
+DPRINT ?= ${LOCAL_BIN}/dprint
+BUN    := bun
 
-BUN := bun --cwd packages/ui
+UI_PATH := packages/ui
 
 start-ui:
-	$(BUN) start
+	$(BUN) --cwd ${UI_PATH} start
 
 dev-ui:
-	$(BUN) dev
+	$(BUN) --cwd ${UI_PATH} dev
 
 docker-build-ui:
-	docker build packages/ui -t minecraft-operator-ui:latest
+	docker build ${UI_PATH} -t minecraft-operator-ui:latest
 
 bin/dprint: .versions/dprint | .make/dprint/install.sh bin
 	DPRINT_INSTALL=${CURDIR} .make/dprint/install.sh $(shell $(DEVCTL) v dprint)
 	@touch $@
 
 bin/devctl: .versions/devctl | bin
-	go install github.com/unmango/devctl/cmd@v$(shell cat $<)
+	GOBIN=${LOCAL_BIN} go install github.com/unmango/devctl/cmd@v$(shell cat $<)
 	mv bin/cmd $@
+
+.envrc: hack/example.envrc
+	cp $< $@
+
+.make/bun-install: ${UI_PATH}/package.json ${UI_PATH}/bun.lock $(shell $(DEVCTL) list --ts)
+	$(BUN) install --cwd ${UI_PATH}
+	@touch $@
